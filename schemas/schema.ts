c356d65269a6e6fb8ef21060c4812cf1012a568f
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2026-04-22 11:38:42 +0500

    Added order type

--- schemas/schema.ts
+++ schemas/schema.ts
@@ -1,10 +1,17 @@
 export type User = {
+  id: string;
   name: string;
   password: string;
   age: number;
 };
 export type Product = {
+  id: string;
   name: string;
   price: string;
   age: number;
+};
+export type Order = {
+  id: string;
+  product_id: string;
+  user_id: string;
 };
